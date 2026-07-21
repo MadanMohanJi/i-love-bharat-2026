2652951a39413495c32e7e45668ee56d6051510d
shivesh <shivesh211204@gmail.com>
2026-07-21 07:00:19 +0530

    Add deployment config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // ADD THIS LINE! It must match your exact repo name surrounded by slashes
-  base: '/i-love-bharat/', 
+  base: '/i-love-bharat-2026/', 
 })
